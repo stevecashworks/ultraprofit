c60abdd57fde328d4eedf88cf14630fdeca7693b
stevecashworks <chigbustephennamdi@gmail.com>
2023-10-02 23:54:19 -0700

    done

--- Routes/Users/admin/adminRoute.js
+++ Routes/Users/admin/adminRoute.js
@@ -1,10 +1,10 @@
 import express from "express"
-import { getStats, setRates } from "./admin_Controller.js"
+import { getRates, getStats, setRates } from "./admin_Controller.js"
 import { verifyAdmin } from "../verify_token.js"
 const adminRoute= express.Router()
 adminRoute.post("/stats",verifyAdmin,getStats)
 adminRoute.post("/setRates",verifyAdmin,setRates)
-adminRoute.get("/getRates",setRates)
+adminRoute.get("/getRates",getRates)
                      
 
 export default adminRoute
